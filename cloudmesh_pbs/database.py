f99f0fac2fe4124fbbf9df384844b2023212086c
Gregor von Laszewski <laszewski@gmail.com>
2015-03-03 13:57:01 -0500

    adding a provider

--- cloudmesh_pbs/database.py
+++ cloudmesh_pbs/database.py
@@ -1,32 +1,41 @@
 import shelve
+import abc
+import os
 
-
-class pbs_db(object):
+class pbs_db_interface(object):
+    __metaclass__  = abc.ABCMeta
     
     def __getitem__(self, id):
         return self.data[id]
         
     def __setitem__(self, id, value):
         self.data[id] = value
-        
-    def get(self, id):
-        pass
     
+    @abc.abstractmethod        
+    def get(self, id):
+        """get the object with the id"""
+    
+    @abc.abstractmethod        
     def set(self, id, value):
-        pass
+        """set the objet with the id to value"""
     
     def clear(self):
-        pass
-    
+        try:
+            os.remove(self.filename)
+        except:
+            pass
+        
+        
+    @abc.abstractmethod        
     def save(self):
-        pass
+        """save the database"""
     
+    @abc.abstractmethod        
     def load(self):
-        pass
+        """load the database"""
     
     
-    
-class pbs_shelve(pbs_db):    
+class pbs_shelve(pbs_db_interface):    
     
     def __init__(self, filename):
         self.filename = filename
@@ -43,9 +52,33 @@ class pbs_shelve(pbs_db):
     
     def set(self, id, value):
         self.data[id] = value
+        
+    def keys(self):
+        self.data.keys()
+        
+    def delete(self, id):
+        del self.data[id]
+
+    def close(self):
+        self.data.close()
+
+class pbs_db(pbs_db_interface):
     
-    def clear(self):
-        os.remove(self.filename)
+    def __init__(self, filename, provider):
+        self.filename = filename
+        self.data = provider(filename)
+    
+    def load(self):
+        self.data.load()
+        
+    def save(self):
+        self.data.sync()
+        
+    def get(self, id):
+        return self.data[id]
+    
+    def set(self, id, value):
+        self.data[id] = value
     
     def keys(self):
         self.data.keys()
@@ -55,3 +88,15 @@ class pbs_shelve(pbs_db):
 
     def close(self):
         self.data.close()
+            
+if __name__ == "__main__":
+    
+    db = pbs_shelve("pbd.shelve")
+    
+    key = "gregor"
+    element = {"name": "gregor", "home": "."}
+    
+    db[key] = element
+    
+        
+    print (db[key])
