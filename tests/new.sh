36031d975b63b509bdb26bfbd02a7ce9981ff0f0
Gregor von Laszewski <laszewski@gmail.com>
2015-03-16 08:52:15 -0400

    added tests

--- tests/new.sh
+++ tests/new.sh
@@ -9,5 +9,21 @@ which python
 pip install cmd3
 pip install cloudmesh_base
 pip install cloudmesh_database
+echo "######################################################################"
+echo "Running tests"
+echo "######################################################################"
 
+mkdir ~/NOSETESTS
 
+cd ~/NOSETESTS
+#
+# git clone git@github.com:cloudmesh/cmd3.git
+# 
+#
+
+cd ~/NOSETESTS
+git clone git@github.com:cloudmesh/pbs.git
+cd pbs/tests
+
+nosetests --nocapture -v database.py
+nosetests --nocapture -v submit.py
