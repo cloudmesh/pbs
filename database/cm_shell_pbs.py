9d8976225760f49b45ee7fe49602ba5b634c19a4
Gregor von Laszewski <laszewski@gmail.com>
2015-05-18 13:12:22 -0400

    adding 3rd party contribution

---
+++ database/cm_shell_pbs.py
@@ -0,0 +1,143 @@
+import cm_mongodb
+
+class cm_shell_jobs:
+	database = None
+
+    def activate_cm_shell_jobs(self):
+        self.register_command_topic('mycommands', 'jobs')
+
+    @command
+    def do_jobs(self, args, arguments):
+        """
+        ::
+cm
+          Usage:
+              jobs REQARG1 OPTARG1 OPTARG2 OPTARG3
+          Performs various jobs functions explained below
+          Arguments:
+            REQARG1     Initial function to perform. Possible values: deploy, start, clear, set, delete, list
+                        startMongo : deploys mongo instance, OPTARG1 is db_path, OPTARG2 is port, OPTARG3 is log_path
+                        stopMongo  : stops mongo instance
+                        connect  : either connects to a pre-existing db or creates a new one
+                        insertJob    : Creates a new job, requires OPTARG1 to be job_name, OPTARG2 & OPTARG3 are input file and output file
+                        findJobs : Finds all running jobs, no required parameters but you may provide key_name and value to return specific jobs
+                        deleteJobs : deletes all running jobs, no required parameters but you may provide key_name and value to delete specific jobs
+						numJobs : prints a count of jobs currently running
+            OPTARG1
+
+            OPTARG2
+
+            OPTARG3
+
+          Options:
+        """
+
+
+        if arguments["REQARG1"] == "startMongo":
+
+			self.database = cm_mongodb.db()
+
+			if arguments["OPTARG1"] is not None:
+				dbpath = arguments["OPTARG1"]
+
+				if argument["OPTARG2"] is not None:
+					dbport = arguments["OPTARG2"]
+
+					if argument["OPTARG3"] is not None:
+						logport = arguments["OPTARG3"]
+						self.database.startMongo(dbpath, dbport, logport)
+
+					else:
+						self.database.startMongo(dbpath, dbport)
+				else:
+					self.database.startMongo(dbpath)
+			else:
+				self.database.startMongo()
+
+
+        elif arguments["REQARG1"] == "stopMongo":
+
+            self.database.stopMongo()
+
+
+        elif arguments["REQARG1"] == "connect":
+
+			if arguments["OPTARG1"] is not None:
+				dbname = arguments["OPTARG1"]
+				self.database.connect(dbname)
+			else:
+				self.database.connect()
+
+        elif arguments["REQARG1"] == "insertJob":
+
+			if argument["OPTARG1"] is not None:
+				jobname = argument["OPTARG1"]
+
+				if argument["OPTARG2"] is not None:
+					input = argument["OPTARG2"]
+
+					if argument["OPTARG3"] is not None:
+						output = argument["OPTARG3"]
+						self.database.insertJobs(jobname, input, output)
+
+					else:
+						self.database.insertJobs(jobname, input)
+				else:
+					self.database.insertJobs(jobname)
+			else:
+				self.database.insertJobs()
+
+        elif arguments["REQARG1"] == "findJobs":
+
+            if argument["OPTARG1"] is not None:
+				keyname = argument["OPTARG1"]
+				value = argument["OPTARG2"]
+				self.database.insertJobs(keyname, value)
+			else:
+				self.database.insertJobs()
+
+
+        elif arguments["REQARG1"] == "deleteJobs":
+
+			if argument["OPTARG1"] is not None:
+				keyname = argument["OPTARG1"]
+				value = argument["OPTARG2"]
+				self.database.deleteJobs(keyname, value)
+            else:
+				self.database.deleteJobs()
+
+		elif arguments["REQARG1"] == "findJobs":
+
+            if argument["OPTARG1"] is not None:
+				keyname = argument["OPTARG1"]
+				value = argument["OPTARG2"]
+				self.database.insertJobs(keyname, value)
+			else:
+				self.database.insertJobs()
+
+
+        elif arguments["REQARG1"] == "numJobs":
+
+			if argument["OPTARG1"] is not None:
+				keyname = argument["OPTARG1"]
+				value = argument["OPTARG2"]
+				self.database.numJobs(keyname, value)
+            else:
+				self.database.numJobs()
+
+		elif arguments["REQARG1"] == "UpdateJobEndTime":
+
+			jobid = argument["OPTARG1"]
+			endtime = argument["OPTARG2"]
+			self.database.numJobs(jobid, endtime)
+
+		elif argument["REQARG1"] == "insertJobObject":
+
+			job = argument["OPTARG1"]
+			self.database.insertJobObject(job)
+
+        pass
+
+if __name__ == '__main__':
+    command = cm_shell_jobs()
+    command.do_jobs()
---
+++ database/test_large.py
@@ -0,0 +1,59 @@
+import cm_mongodb
+
+db = cm_mongodb.db()
+
+db.startMongo()
+
+#Connect to test database
+print "Connecting to local database"
+
+db.connect("newdb")
+
+#Insert 20 jobs
+#   Use different job names but identical input and output filenames for later queries
+print "\nInserting all jobs"
+
+for i in range(0, 20):
+    db.insertJob("job" + str(i), "input", "output")
+
+#Print out all jobs
+print "\nAll Jobs:"
+
+for job in db.findJobs():
+    print job
+
+#Print all jobs with job name as "job2"
+#Available key names at this time are:
+#   _id
+#   job_name
+#   input_filename
+#   output_filename
+print "\nJob with Job Name as 'job2'"
+
+for job in db.findJobs("job_name", "job2"):
+    print job
+
+#Print out count of all jobs
+numJobs = db.numJobs()
+print "\nNumber of Total Jobs: " + str(numJobs)
+
+#Print out count of all jobs with job name as "job2"
+numJobs2 = db.numJobs("job_name", "job2")
+print "\nNumber of Jobs with Job Name as 'job2': " + str(numJobs2)
+
+#Delete all jobs with job name as "job2"
+db.deleteJobs("job_name", "job2")
+
+#Print out count of all jobs with job name as "job2"
+print "\nDeleting all jobs with job name as 'job2'"
+
+numJobs2 = db.numJobs("job_name", "job2")
+print "Number of Jobs with Job Name as 'job2': " + str(numJobs2)
+
+#Delete all jobs
+db.deleteJobs()
+
+#Print out count of all jobs
+print "\nDeleting all remaining jobs"
+numJobs = db.numJobs()
+print "Number of Total Jobs: " + str(numJobs)
