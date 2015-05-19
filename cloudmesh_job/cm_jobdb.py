eb6e07e4dde47b64a80f81c628a89f425c24eb55
Gregor von Laszewski <laszewski@gmail.com>
2015-05-19 06:59:47 -0400

    moving parameters to the yaml file

--- cloudmesh_job/cm_jobdb.py
+++ cloudmesh_job/cm_jobdb.py
@@ -17,6 +17,14 @@ class job_db (object):
     database = None
     jobs = None
 
+    def load(self, filename="/cloudmesh_pbs.yaml"):
+        self.filename = config_file(filename)
+        self.data = ConfigDict(filename=filename)
+
+        self.port = self.data["cloudmesh"]["jobdatabase"]["port"]
+        self.log_file = self.data["cloudmesh"]["jobdatabase"]["log"]
+        self.db_file = self.data["cloudmesh"]["jobdatabase"]["filename"]
+
     def __init__(self, deploy=True, yaml_filename="/cloudmesh_pbs.yaml"):
         """
         Creates an object instance of a job database as defined in the cloudmesh_pbs yaml file
@@ -26,41 +34,40 @@ class job_db (object):
                               cloudmesh_pbs.yaml
         :return: an object for manageing jobs in the database
         """
-        self.yaml_filename = config_file(yaml_filename)
 
-        self.pbs_dir = config_file("/pbs")
         self.id_file = config_file("/pbs/id.txt")
         self.db_file = config_file("/pbs/job.db")
+        self.job_dir = os.path.dirname(self.db_file)
 
-        ## TODO see DbPBS
-        # if deploy:
-        #    self.deploy()
-        #self.load()
-        #self.id = self.jobid
+        self.load(filename=yaml_filename)
 
-        #self.pbs_nodes_data = None
+        if deploy:
+            self._deploy()
+
+    def _deploy(self):
+        """
+        creates the directories if they do not exist
+        :return:
+        """
+        try:
+            for file in [self.db_path, self.log_path]:
+                r = Shell.mkdir(os.path.dirname(file))
+        except Exception, e:
+            Console.error("Problem creationg the database directory")
+            print(e)
 
     # TODO COMBINE WITH __INIT__ ???
     # BUG not a good location for  the data /data
     # BUG not a good location for logpath as we run this in user mode
-    def start(self,
-              db_path="/pbs/",  # will be in ~/.cloudmesh/pbs
-              port="27018",  # will be in ~/.cloudmesh/pbs/lob
-              log_path="/pbs/log/"):
+    # port and location should probably read from cloudmesh_pbs.yaml and values here set to None.see load function
 
-        #Create the data path and log path in case they do not exist
-        Shell.mkdir(db_path)
-        Shell.mkdir(log_path)
-
-        #Deploy the mongoDB
-        # use Shell
+    def start(self)
         try:
-
             r = Shell._execute("mongod",
-                               "--dbpath", db_path,
-                               "--port", port,
+                               "--dbpath", self.db_path,
+                               "--port", self.port,
                                "--fork",
-                               "--logpath", log_path)
+                               "--logpath", self.log_path)
 
         except Exception, e:
             Console.error("we had a problem starting the  mongo daemon")
@@ -68,7 +75,8 @@ class job_db (object):
 
         Console.ok("MongoDB has stopped")
 
-        Console.ok("MongoDB has been deployed at path {:} on port {:} with log {:}".format(db_path, port, log_path))
+        Console.ok("MongoDB has been deployed at path {:} on port {:} with log {:}"
+                   .format(db_path, port, log_path))
 
     def stop(self):
         try:
