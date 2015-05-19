b26dcd4d7a27096620457ca8f4396c97e11a162b
Gregor von Laszewski <laszewski@gmail.com>
2015-05-19 09:45:51 -0400

    improve the yaml file

--- cloudmesh_job/cm_jobdb.py
+++ cloudmesh_job/cm_jobdb.py
@@ -11,7 +11,8 @@ import pymongo
 from pymongo import MongoClient
 import datetime
 import os
-
+from pprint import pprint
+import sys
 
 class JobDB(object):
     database = None
@@ -22,11 +23,11 @@ class JobDB(object):
         self.data = ConfigDict(filename=self.filename)
 
         self.port = self.data["cloudmesh"]["jobdatabase"]["port"]
-        self.log_file = self.data["cloudmesh"]["jobdatabase"]["log"]
-        self.db_file = self.data["cloudmesh"]["jobdatabase"]["filename"]
-        self.job_dir = os.path.dirname(self.db_file)
+        self.db_path = self.data["cloudmesh"]["jobdatabase"]["db_path"]
+        self.log_path = self.data["cloudmesh"]["jobdatabase"]["log_path"]
+        self.dbname = self.data["cloudmesh"]["jobdatabase"]["dbname"]
 
-    def __init__(self, deploy=True, yaml_filename="/cloudmesh_pbs.yaml"):
+    def __init__(self, yaml_filename="/cloudmesh_pbs.yaml"):
         """
         Creates an object instance of a job database as defined in the cloudmesh_pbs yaml file
 
@@ -36,20 +37,18 @@ class JobDB(object):
         :return: an object for manageing jobs in the database
         """
         self.load(filename=yaml_filename)
+        self.deploy()
 
-        if deploy:
-            self._deploy()
-
-    def _deploy(self):
+    def deploy(self):
         """
         creates the directories if they do not exist
         :return:
         """
         try:
-            for file in [self.db_file, self.log_file]:
-                r = Shell.mkdir(os.path.dirname(file))
+            for file in [self.db_path, self.log_path]:
+                r = Shell.mkdir(file)
         except Exception, e:
-            Console.error("Problem creationg the database directory")
+            Console.error("Problem creating the database directory {:}".format(file))
             print(e)
 
     # TODO COMBINE WITH __INIT__ ???
@@ -59,20 +58,20 @@ class JobDB(object):
 
     def start(self):
         try:
-            r = Shell._execute("mongod",
-                               "--dbpath", self.db_file,
-                               "--port", self.port,
-                               "--fork",
-                               "--logpath", self.log_file)
+            r = Shell.mongod("--dbpath", self.db_path,
+                             "--port", self.port,
+                             "--fork",
+                             "--logpath", self.log_path)
+            Console.ok("MongoDB has been deployed at path {:} on port {:} with log {:}"
+                       .format(self.db_path, self.port, self.log_path))
 
         except Exception, e:
             Console.error("we had a problem starting the  mongo daemon")
             print(e)
+            Console.error("MongoDB has stopped")
+            # TODO remove the exit in final code, for debugging only
+            sys.exit()
 
-        Console.ok("MongoDB has stopped")
-
-        Console.ok("MongoDB has been deployed at path {:} on port {:} with log {:}"
-                   .format(db_file, port, log_file))
 
     def stop(self):
         try:
@@ -86,9 +85,10 @@ class JobDB(object):
     def info(self):
         # TODO: implement self. dbath, self.port, self.logpath
         Console.ok("Mongo parameters")
-        Console.ok("  dbpath:", self.db_file)
-        Console.ok("  port:", self.port)
-        Console.ok("  port:", self.log_file)
+        Console.ok("  dbpath:  {:}".format(self.db_path))
+        Console.ok("  port:    {:}".format(self.port))
+        Console.ok("  logfile: {:}".format(self.log_path))
+        Console.ok("  dbname:  {:}".format(self.dbname))
 
     def connect(self, db_name):
 
@@ -100,19 +100,19 @@ class JobDB(object):
         Console.info("Connecting to the Mongo Database")
 
     def insert(self,
-               host,
                job_name,
-               job_group,
-               job_label,
-               job_id,  # possibly same as job_name ?
                input_filename="",
                output_filename="",
+               job_group=None,
+               job_label=None,
+               job_id=None,  # possibly same as job_name ?
+               host=None,
                start_time=str(datetime.datetime.now()),
                end_time=str(datetime.datetime.now()),
                update_time=str(datetime.datetime.now()),
                job_status="C_DEFINED",  # see what we wrote in paper
 ):
-        # TODO: end_teme should be None as unlikely
+        # TODO: end_time should be None as unlikely
         # it so short and we need to deal with jobs
         # that we do not know how long they are
 
@@ -123,11 +123,17 @@ class JobDB(object):
                    "job_group": job_group,
                    "job_label": job_label,
                    "job_status": job_status,
+                   "host": host,
                    "input_filename": input_filename,  # must be array
                    "output_filename": output_filename,  # must be array
                    "start_time": start_time,
-                   "end_time": end_time
+                   "end_time": end_time,
+                   "update_time": update_time,
                    }
+
+            banner("job")
+            pprint(job)
+            banner("insert")
 
             job_id = self.jobs.insert_one(job).inserted_id
 
