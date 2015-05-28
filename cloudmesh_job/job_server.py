edbd1730966c3e16cbca48a58453f6640b16ebda
kwtillma <kwtillma@gmail.com>
2015-05-27 23:30:19 -0500

    Update job_server.py
    I have replaced the contents of job_server.py to reflect REST calls that will add or delete a job by job name. Let me know if this along the right path. I have the old copy of this file stored as api.py on my local machine.

--- cloudmesh_job/job_server.py
+++ cloudmesh_job/job_server.py
@@ -1,58 +1,31 @@
-from flask import Flask
-import cm_generate_command
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from cloudmesh_job.cm_jobdb import JobDB
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/start')
-def start():
-	return 'Starting Mongo'
+db = JobDB()
 
-@app.route('/stop')
-def stop():
-	return 'Stopping Mongo'
+'''will insert job by job_id'''
+class Insert(Resource):
+    def put(self, args_job_name):
+	db.insert(args_job_name)
+	print "will now search for job, if properly inserted, will print out value"
+        return db.find_jobs({"job_name" : args_job_name})
 
-@app.route('/info')
-def info():
-	return 'Showing info'
+class Delete(Resource):
+    def delete(self, args_job_name):
+	'''if job exists, delete'''
+        if db.find_jobs(args_job_name): 
+		db.delete(args_job_name)
+		return Console.ok("job has been deleted")
+	else:
+		return Console.ok("Job not found")
 
-@app.route('/connect')
-def connect():
-	return 'Connecting to DB'
+api.add_resource(Insert, '/insert/<string:job_name>')
+api.add_resource(Delete, '/delete/<string:job_name>')
 
-@app.route('/modify')
-def modify():
-	return 'Modifying'
-
-@app.route('/add')
-def add():
-	return 'adding'
-
-@app.route('/insert')
-def insertJob():
-	return 'Inserting Job'
-
-@app.route('/find')
-def findJobs():
-	return 'finding jobs'
-
-@app.route('/delete')
-def delete():
-	return 'Deleting jobs'
-
-@app.route('/Count')
-def startMongo():
-	return 'counting jobs'
-
-@app.route('/update_job_end_time')
-def update_job_end_time():
-	return 'Starting update_job_end_time'
-
-@app.route('/updateJobAttributes')
-def updateJobAttributes():
-	return 'Starting updateJobAttributes'
-
-@app.route('/jobStatusStats')
-def jobStatusStats():
-	return 'Starting jobStatusStats'
-
+'''debug should not be in production code'''
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) 
